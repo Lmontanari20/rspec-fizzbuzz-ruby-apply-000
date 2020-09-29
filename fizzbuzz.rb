e7bce2348deb4f8cb841a9d81065cfa795c4bbb0
Lucas Montanari <lmonty2011@gmail.com>
2020-09-29 20:08:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
-    return "Fizz"
+    return "FizzBuzz"
   elsif num % 5 == 0
     return "Buzz"
   elsif num % 3 == 0 
-    return "FizzBuzz"
+    return "Fizz"
   else 
     return nil
   end
